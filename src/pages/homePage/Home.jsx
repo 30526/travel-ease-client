4fe385cec6c389f72fa3fc37c81d702d24163bb1
30526 <taiburrahamanapu@gmail.com>
2025-12-29 15:26:36 +0600

    created useAuth, and useAxios hooks

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div className='min-h-screen'>
-            This is Home page
-        </div>
-    );
+  return <div className="min-h-screen">This is Home page</div>;
 };
 
 export default Home;
